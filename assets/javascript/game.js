d68c57d34d5e13eb72236c86a8415b44a439f2f2
skanakia <skanakia1@gmail.com>
2018-05-21 23:07:13 -0400

    built the game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,28 @@
-var charHp = 0;
+var charHP = 0;
 var oppHp = 0;
 var attPow = 0;
+var attPowBase = 0;
 var countAttPow = 0; 
 var charSelected = false;
 var oppSelected = false;
 var allSelected = false;
 var force = "";
-var enemiesDown = 0;
+var charName = "";
+var defDiv = "";
+var gameLost = false;
+var oppDown = 0;
+var gameWon = false;
 
 function isSelected(){
     if (charSelected === false) {
-        return "please select your character."
+        return "please select your character.";
     } else if (oppSelected === false) {
-        return "please select your opponent."
+        return "please select your opponent.";
     } else {
         allSelected = true;
     }
 }
+
 
 $(document).ready(function() {
     $(".char").on("mouseenter", function() {
@@ -33,32 +39,36 @@ $(document).ready(function() {
             $("#fighter2").appendTo($("#opp-box"));
             $("#fighter3").appendTo($("#opp-box"));
             $("#fighter4").appendTo($("#opp-box"));
+            charName = "Obi-Wan Kenobi";
             charHP = 120;
-            attPow = 8;
+            attPowBase = 8;
             force = "light";
 
         } else if ($(this).attr('id') === "fighter2") {
             $("#fighter1").appendTo($("#opp-box"));
             $("#fighter3").appendTo($("#opp-box"));
             $("#fighter4").appendTo($("#opp-box"));
+            charName = "Luke Skywalker";
             charHP = 100;
-            attPow = 10;
+            attPowBase = 10;
             force = "light";
 
         } else if ($(this).attr('id') === "fighter3") {
             $("#fighter1").appendTo($("#opp-box"));
             $("#fighter2").appendTo($("#opp-box"));
             $("#fighter4").appendTo($("#opp-box"));
+            charName = "Darth Insidious";
             charHP = 150;
-            attPow = 7;
+            attPowBase = 7;
             force = "dark";
 
         } else if ($(this).attr('id') === "fighter4") {
             $("#fighter1").appendTo($("#opp-box"));
             $("#fighter2").appendTo($("#opp-box"));
             $("#fighter3").appendTo($("#opp-box"));
+            charName = "Darth Maul";
             charHP = 180;
-            attPow = 6;
+            attPowBase = 6;
             force = "dark";
 
         } else {
@@ -72,8 +82,42 @@ $(document).ready(function() {
 
     $("#opp-box").on("click", ".char", function(){
         console.log("working");
+        defDiv = $(this);
         if (oppSelected === false) {
             $(this).appendTo("#defender-box");
+            if ($(this).attr('id') === "fighter1") {
+                oppName = "Obi-Wan Kenobi";
+                oppHp = 120;
+                if (force === "light") {
+                    countAttPow = 6;
+                } else {
+                    countAttPow = 60;
+                }
+            } else if ($(this).attr('id') === "fighter2") {
+                oppName = "Luke Skywalker";
+                oppHp = 100;
+                if (force === "light") {
+                    countAttPow = 5;
+                } else {
+                    countAttPow = 40;
+                }
+            } else if ($(this).attr('id') === "fighter3") {
+                oppName = "Darth Insidious";
+                oppHp = 150;
+                if (force === "dark") {
+                    countAttPow = 5;
+                } else {
+                    countAttPow = 25;
+                }
+            } else {
+                oppName = "Darth Maul";
+                oppHp = 180;
+                if (force === "dark") {
+                    countAttPow = 5;
+                } else {
+                    countAttPow = 25;
+                }
+            }
         } else {
 
         }
@@ -81,7 +125,106 @@ $(document).ready(function() {
     });
 
     $("#fightButton").on("click", function() {
-        console.log("working");
+        $("#fight-dialog").empty();
+        isSelected();
+        if (allSelected === true && gameLost === false) {
+            console.log("true");
+            var newP = $("<p>");
+            var newP2 = $("<p>");
+            attPow = attPow + attPowBase;
+            oppHp = oppHp - attPow;
+            if (oppHp <= 0) {
+                $("#defender-box").empty()
+                oppSelected = false;
+                allSelected = false;
+                oppDown++;
+                if (oppDown === 3) {
+                    gameWon = true;
+                    var newDiv = $("<div>");
+                    $(newDiv).text("You Won. Please hit the Reset Button to Play Again.");
+                    $("#fight-dialog").append(newDiv);
+                }                
+                var newP7 = $("<p>");
+                $(newP7).text("You killed " + oppName);
+                $("#fight-dialog").append(newP7);
+            } else {
+
+            }
+            
+            if (allSelected === true) {
+                charHP = charHP - countAttPow; 
+            } else {
+                charHP = charHP;
+            }
+
+            if (charHP <= 0) {
+                var newP5 = $("<p>");
+                $(newP5).text("You Lose, press restart to play again.");
+                $("#fight-dialog").append(newP5);
+                gameLost = true;
+            } else {
+
+            }
+            $(newP).text("You attacked " + oppName + " for " + attPow + " damage.");
+            $(newP2).text(oppName + " attacked you for " + countAttPow + " damage.");
+            $("#fight-dialog").append(newP);
+            $("#fight-dialog").append(newP2);
+            if (oppName === "Obi-Wan Kenobi") {
+                $("#hp120").text(oppHp);
+            } else if (oppName === "Luke Skywalker") {
+                $("#hp100").text(oppHp);
+            } else if (oppName === "Darth Insidious") {
+                $("#hp150").text(oppHp);
+            } else if (oppName === "Darth Maul") {
+                $("#hp180").text(oppHp);
+            } else {
+
+            }
+            if (charName === "Obi-Wan Kenobi") {
+                $("#hp120").text(charHP);
+            } else if (charName === "Luke Skywalker") {
+                $("#hp100").text(charHP);
+            } else if (charName === "Darth Insidious") {
+                $("#hp150").text(charHP);
+            } else if (charName === "Darth Maul") {
+                $("#hp180").text(charHP);
+            } else {
+
+            }
+        } else if (gameLost === true) {
+            var newP3 = $("<p>");
+            $(newP3).text("You Lose, press restart to play again.");
+            $("#fight-dialog").append(newP3);
+        } else if (gameWon === true) {
+            var newDiv2 = $("<div>");
+            $(newDiv2).text("You Won. Please hit the Reset Button to Play Again.");
+            $("#fight-dialog").append(newDiv2);
+        } else {
+            var error = isSelected();
+            var newP4 = $("<p>");
+            $(newP4).text(error);
+            $("#fight-dialog").append(newP3);
+        }
     });
 
+    $("#resetButton").on("click", function() {
+        var charHP = 0;
+        var oppHp = 0;
+        attPow = 0;
+        attPowBase = 0;
+        countAttPow = 0; 
+        charSelected = false;
+        oppSelected = false;
+        allSelected = false;
+        force = "";
+        charName = "";
+        defDiv = "";
+        gameLost = false;
+        oppDown = 0;
+        gameWon = false;
+
+        location.reload();
+    });
+
+   
 });
